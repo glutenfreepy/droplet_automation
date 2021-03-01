3d6bf50746354b6de14e5ba917470da07a57f0a6
edg <glutenfreepy@gmail.com>
2021-02-28 17:00:48 -0800

    start watch folder and outline file processing steps

--- script.py
+++ script.py
@@ -6,8 +6,23 @@ suffix = 'pdf'
 files = os.listdir(path) # get list of files in the folder
 filepath = f'{path}{f}'
 
-for f in files:
-    if filepath.endswith(suffix):
-        open -a dropletpath filepath
-    else:
-        print("File skipped since it is not a pdf.")
+def run_droplet():
+    for f in files:
+        if filepath.endswith(suffix):
+            open -a dropletpath filepath
+        else:
+            print("File skipped since it is not a pdf.")
+
+def watch_folder():
+
+
+    # get a list of folders
+    watchfolder = "/path/to/watchfolder"
+    folderlist = os.listdir(watchfolder)
+
+    # get name of folder from list and see PSD-DONE folder exists
+    pdffolder = folderlist
+
+    # convert files
+
+    # create new folder <foldername_PSD-DONE> and move psds to it
