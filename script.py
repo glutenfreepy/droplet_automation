961d247b144c6d172c1055f935c8aad85e7968f7
Ed Garcia <ed.garcia@turner.com>
2021-03-06 19:35:25 -0800

    add endswith(), get rid of extra function

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import os
-import re
+
 
 watch_folder_path = './testwatchfolder/'
 folder_list = os.listdir(watch_folder_path) # get list of files in the folder
@@ -14,30 +14,39 @@ def watch_folder():
     print(f"i see these folders in the watch folder: {folder_list}")
     print()
     for folder in folder_list:
-        if 'PSDCONVERT' in folder: # TODO: add logic to look for endswith -DONE
-            print(folder)
+        if folder.endswith("-DONE"):
+            print(f"skipping DONE folder {folder}")
+            continue
+
+        elif 'PSDCONVERT' in folder:
+            print(f"converting {folder}")
             # set working folder path
             working_folder_path = f"{watch_folder_path}{folder}"
+
             print(f"the working folder path is: {working_folder_path}")
 
-            # convert files
-            run_droplet(working_folder_path)
+            files = os.listdir(working_folder_path)
 
-            # TODO: rename folder with foldername-DONE
+            print(f"checking these files for pdfs: {files}")
+
+            # run droplet to convert files to psd
+            for file in files:
+                if file.endswith(suffix):
+                    filepath = f"{working_folder_path}/{file}"
+                    # TODO: run droplet
+                    print(f"running droplet on {filepath}")
+                    # "open -a dropletpath filepath"
+
+                else:
+                    print(f"{file} was skipped since it is not a pdf.")
+
+            # append "-DONE" to foldername
+            print("appending '-DONE' to foldername")
             os.rename(working_folder_path, working_folder_path+"-DONE")
-        else:
-            print('skipped')
 
-
-def run_droplet(pdffolder):
-    for f in pdffolder:
-        if f.endswith(suffix):
-            filepath = f"{pdffolder}{f}"
-            print(filepath)
-            # TODO: rn droplet
-            # "open -a dropletpath filepath"
         else:
-            print("File skipped since it is not a pdf.")
+            print(f'skipped folder {folder} - no PSDCONVERT found')
+
 
 
 watch_folder()
